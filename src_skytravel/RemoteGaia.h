170f7543b3c6c98346aa5a1b6f866fd86252385c
Mark Sproul <msproul@skychariot.com>
2022-01-17 10:01:55 -0500

    More files

---
+++ src_skytravel/RemoteGaia.h
@@ -0,0 +1,10 @@
+//#include	"RemoteGaia.h"
+
+
+//*	these are read in from a config file "gaiasql.txt"
+extern	char	gGaiaSQLsever_IPaddr[];
+extern	int		gGaiaSQLsever_Port;
+extern	char	gGaiaSQLsever_UserName[];
+extern	char	gGaiaSQLsever_Password[];
+extern	char	gGaiaSQLsever_Database[];
+extern	int		gGaiaSQLerror_Count;
